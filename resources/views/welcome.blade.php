39aff0d0b95ad96cb0d5bff98ca90e201f5bdf1d
MuchiriKinyua <muchirikinyua2000@gmail.com>
2025-07-04 13:34:59 +0300

    welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Call Center | Welcome</title>
+    <title>Call Center</title>
 
     <!-- Fonts -->
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
